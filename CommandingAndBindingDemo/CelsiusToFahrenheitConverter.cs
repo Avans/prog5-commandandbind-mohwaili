3353c6ecb2aa6658b4dd70e0f2d71417bf4caaae
mohwaili <mohammed.alwaili@gmail.com>
2016-09-24 13:19:24 +0200

    add celsius to fahrenheit converter

---
+++ CommandingAndBindingDemo/CelsiusToFahrenheitConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CommandingAndBindingDemo
+{
+    class CelsiusToFahrenheitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double result = (double)value;
+            return result * 9 / 5 + 32;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
---
+++ CommandingAndBindingDemo/SetHighCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CommandingAndBindingDemo
+{
+    class SetHighCommand : ICommand
+    {
+        private TemperatureVM _viewModel;
+
+        public SetHighCommand(TemperatureVM viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _viewModel != null && _viewModel.Temperature < 200;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _viewModel.Temperature = 300;
+        }
+    }
+}
